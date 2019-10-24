c6f5944aab85217ac243b2bd874db1575b709dd4
Xavier Coulon <xcoulon@redhat.com>
2019-10-24 15:15:39 +0200

    add version
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ version/version_test.go
@@ -0,0 +1,16 @@
+package version_test
+
+import (
+	"testing"
+
+	"github.com/codeready-toolchain/operator-role-linter/version"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVars(t *testing.T) {
+	// simply verify that the vars exist.
+	// They will be populated by the `-ldflags="-X ..."` at build time
+	assert.Equal(t, "unknown", version.Commit)
+	assert.Equal(t, "unknown", version.BuildTime)
+}
